3f671005458013de789586ffa382181358e9bf46
nordyke <aaron.nordyke@gmail.com>
2016-12-01 13:12:23 -0600

    alias fix

--- c2fo/c2fo.zsh
+++ c2fo/c2fo.zsh
@@ -10,12 +10,12 @@ export SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -X
 
 # Docker-compose
 alias compose="docker-compose -p backing-apps -f $C2FO_HOME/docker-compose.yml"
-alias compose-prod="docker-compose -p backing-apps -f $C2FO_HOME/docker-compose.yml -f $C2FO_HOME/docker-compose.             proddump.yml"
-alias compose-testdata="docker-compose -p backing-apps -f $C2FO_HOME/docker-compose.yml -f $C2FO_HOME/docker-compose.         testdata.yml"
+alias compose-prod="docker-compose -p backing-apps -f $C2FO_HOME/docker-compose.yml -f $C2FO_HOME/docker-compose.proddump.yml"
+alias compose-testdata="docker-compose -p backing-apps -f $C2FO_HOME/docker-compose.yml -f $C2FO_HOME/docker-compose.testdata.yml"
 alias c2fo_migrate="(cd $C2FO_HOME && (NODE_ENV=dockertest grunt migrate) && grunt migrate)"
 alias compose-reset="compose down && docker-clean run && compose up -d && sleep 5 && c2fo_migrate"
 alias compose-prod-reset="compose-prod down && docker-clean run && compose-prod up -d && sleep 5 && c2fo_migrate"
-alias compose-testdata-reset="compose-testdata down && docker-clean run && compose-testdata up -d && sleep 5 &&               c2fo_migrate"
+alias compose-testdata-reset="compose-testdata down && docker-clean run && compose-testdata up -d && sleep 5 && c2fo_migrate"
 
 alias docker-stop-all="docker stop $(docker ps -a -q)"
 

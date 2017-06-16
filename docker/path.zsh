57bf4bf001943f3bdb2c1b56993b87bf01cd2f08
nordyke <aaron.nordyke@gmail.com>
2017-06-16 14:51:42 -0500

    docker

--- docker/path.zsh
+++ docker/path.zsh
@@ -1,3 +1,3 @@
- export DOCKER_SCRIPTS=/Users/aaron.nordyke/docker/scripts
- export DOCKER_HOST=tcp://172.17.8.150:2375
- export PATH=$PATH:$DOCKER_SCRIPTS
+export DOCKER_SCRIPTS=/Users/aaron.nordyke/docker/scripts
+export DOCKER_HOST=tcp://172.17.8.150:2375
+export PATH=$PATH:$DOCKER_SCRIPTS

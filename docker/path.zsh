8fcaa0c8cc7ce22b035d016251a8c09887effefb
nordyke <aaron.nordyke@gmail.com>
2017-08-28 14:10:39 -0500

    gpg autosign commits

--- docker/path.zsh
+++ docker/path.zsh
@@ -1,4 +1,4 @@
 export DOCKER_SCRIPTS=/Users/aaron.nordyke/docker/scripts
-#export DOCKER_API_VERSION=1.24
-#export DOCKER_HOST=tcp://172.17.8.150:2375
+export DOCKER_API_VERSION=1.12.6
+export DOCKER_HOST=tcp://172.17.8.150:2375
 export PATH=$PATH:$DOCKER_SCRIPTS

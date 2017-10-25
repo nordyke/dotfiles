9c87699566c3f5de023955c8456f388156f03d62
nordyke <aaron.nordyke@gmail.com>
2017-10-25 11:28:26 -0500

    gpg-agent

--- c2fo/aliases.zsh
+++ c2fo/aliases.zsh
@@ -1,1 +1,5 @@
 alias pull-go="~/c2fo/pull-go.sh"
+alias calculator_migrate="cd ~/Documents/akka-calc && sbt 'db clean' db && cd --"
+alias user_auth_migrate="cd $USER_AUTH_HOME && npm run migrate:up && cd --"
+
+alias db_migrate="c2fo_migrate && calculator_migrate && user_auth_migrate"
--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -4,6 +4,5 @@ alias compose-testdata="docker-compose -p backing-apps -f $C2FO_HOME/docker-comp
 alias c2fo_migrate="(cd $C2FO_HOME && (NODE_ENV=dockerdev grunt migrate) && grunt migrate)"
 alias compose-reset="compose down && docker-clean run && compose up -d && sleep 5 && c2fo_migrate"
 alias compose-prod-reset="compose-prod down && docker-clean run && compose-prod up -d && sleep 5 && c2fo_migrate"
-alias compose-testdata-reset="compose-testdata down && docker-clean run && compose-testdata up -d && sleep 5 && c2fo_migrate"
- 
+alias compose-testdata-reset="compose-testdata down && docker-clean run && compose-testdata up -d && sleep 5 && c2fo_migrate && user_auth_migrate"
 alias docker-stop-all="docker stop $(docker ps -a -q)"
